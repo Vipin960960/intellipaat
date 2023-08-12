d4039ec86057f038c44637d906c5c43659c9a677
Vipin Kumar <68015019+Vipin960960@users.noreply.github.com>
2023-08-12 11:20:36 +0530

    Add files via upload

---
+++ netflix_clone/public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "606885cd6c3b3db68273332918f91850";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
